ed63c8c795f9e812d08f999eea65ffd69ea4df24
uvivi161 <uvi69161@gmail.com>
2024-12-14 18:59:58 +0200

    h.w. add validation to the form

--- src/components/teachersForm/form/form.component.ts
+++ src/components/teachersForm/form/form.component.ts
@@ -15,10 +15,12 @@ import { UseIconPipeComponent } from "../../use-icon-pipe/use-icon-pipe.componen
 export class FormComponent {
   teachers:Teacher[] =[];
   teach : Teacher= new Teacher(9999,"ttttttttttttt",["c1","c4"]);
+  teach1 : Teacher= new Teacher(9999,"ttttttttttttt",["c1","c4"]);
+
   constructor(private teacherService :TeacherService) {
     this.teachers= teacherService.getTeachers();
   }
-  onSubmitAdd() {
+  onSubmitAdd() {  
     this.teacherService.addTeacher(this.teach.name, this.teach.classes);
   }
   onSubmitUpdate(){
